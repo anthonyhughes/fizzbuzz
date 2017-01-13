3d7aa5c543db6ab143c5f6b111b6f85d112b1716
ra428 <rajiv.kurien@zuhlke.com>
2017-01-13 10:57:06 +0000

    Replaces multiples of five with Buzz

--- src/main/java/ReplaceFiveWithBuzz.java
+++ src/main/java/ReplaceFiveWithBuzz.java
@@ -1,4 +1,13 @@
 public class ReplaceFiveWithBuzz {
 
 
+    public ReplaceFiveWithBuzz() {
+    }
+
+    public String replace(int number) {
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return Integer.toString(number);
+    }
 }
--- src/test/java/ReplaceFiveWithBuzzTest.java
+++ src/test/java/ReplaceFiveWithBuzzTest.java
@@ -1,10 +1,17 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ReplaceFiveWithBuzzTest {
 
     @Test
-    public void shouldPass(){
-
+    public void replaceFiveWithBuzz(){
+        assertEquals("Buzz", new ReplaceFiveWithBuzz().replace(5));
     }
 
+    @Test
+    public void replacesOnlyMultiplesOfFiveWithBuzz(){
+        assertEquals("1", new ReplaceFiveWithBuzz().replace(1));
+
+    }
 }
