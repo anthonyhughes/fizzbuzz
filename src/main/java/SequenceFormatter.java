a18cebb78513e2dd754e5eda229a4c4417f594db
Anthony Hughes <anthony.hughes@datalanguage.com>
2017-01-13 11:01:24 +0000

    added comma separated integer formatter

--- src/main/java/SequenceFormatter.java
+++ src/main/java/SequenceFormatter.java
@@ -2,4 +2,18 @@
  * Created by anthonyhughes on 13/01/2017.
  */
 public class SequenceFormatter {
+
+    public String format(int... integers) {
+        String output = "";
+        for (int i = 0; i < integers.length; i++) {
+            if (i + 1 == integers.length) {
+                output = output.concat(Integer.toString(integers[i]));
+            } else {
+                output = output.concat(integers[i] + ",");
+            }
+
+        }
+        return output;
+    }
+
 }
--- src/test/java/SequenceFormatterTest.java
+++ src/test/java/SequenceFormatterTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by anthonyhughes on 13/01/2017.
  */
 public class SequenceFormatterTest {
+
+    @Test
+    public void formatInteger() {
+        assertEquals("0", new SequenceFormatter().format(0));
+    }
+
+    @Test
+    public void formatIntegers() {
+        assertEquals("0,1,2", new SequenceFormatter().format(0, 1, 2));
+    }
+
 }
