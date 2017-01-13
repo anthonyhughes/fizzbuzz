42a05f64ebbea991a593f63bd192e8a1943fef1c
Anthony Hughes <anthony.hughes@datalanguage.com>
2017-01-13 11:32:38 +0000

    updated replace fifteen fizz buzz

---
+++ src/main/java/ReplaceFifteenWithFizzBuzz.java
@@ -0,0 +1,10 @@
+/**
+ * Created by anthonyhughes on 13/01/2017.
+ */
+public class ReplaceFifteenWithFizzBuzz {
+
+    public String replace(int input) {
+        return input % 15 == 0 ? "FizzBuzz" : input + "";
+    }
+
+}
--- src/test/java/ReplaceFifteenWithFizzBuzzTest.java
+++ src/test/java/ReplaceFifteenWithFizzBuzzTest.java
@@ -6,17 +6,16 @@ import static org.junit.Assert.assertEquals;
  * Created by capi on 13/01/2017.
  */
 public class ReplaceFifteenWithFizzBuzzTest {
+
     @Test
     public void fifteenIsFizzBuzz() {
-        assertEquals("FizzBuzz", fizzBuzz(15));
+        assertEquals("FizzBuzz", new ReplaceFifteenWithFizzBuzz().replace(15));
 
     }
+
     @Test
     public void oneIsOne() {
-        assertEquals("1", fizzBuzz(1));
+        assertEquals("1", new ReplaceFifteenWithFizzBuzz().replace(1));
     }
 
-    private String fizzBuzz(int input) {
-        return input % 15 == 0 ? "FizzBuzz" : input+"";
-    }
 }
